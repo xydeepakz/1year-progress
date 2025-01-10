954d28a531d5ae00614c50075fb48ac9978a05d5
xydeepakz <xydeepakz@gmail.com>
2025-01-10 19:18:49 +0530

    new_project_1

---
+++ Project1/script.js
@@ -0,0 +1,7 @@
+function ball() {
+    var sun = document.getElementById('ball');
+    sun.style.visibility = "visible";
+    sun.style.animation = "ball 5s ease-in-out infinite";
+    var btn = document.getElementById('btn');
+    btn.style.visibility = "hidden";
+}
